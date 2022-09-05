5c240c0e6d6634ac4a16a798c2f005c48528fdeb
Joanna <j.puno0909@gmail.com>
2022-09-05 14:35:50 -0600

    remove old setup

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+//  import Portfolio from '@/views/Portfolio.vue'
+</script>
+
+<template>
+  <!-- <Portfolio /> -->
+  hello
+</template>
--- src/axios-orders.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-
-const instance = axios.create({
-    baseURL: 'https://react-burger-builder-10f18.firebaseio.com/'
-})
-
-export default instance;

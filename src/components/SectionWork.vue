e66e747ea6c92d49d525db82fa608555a1c5fc54
Joanna <j.puno0909@gmail.com>
2023-01-31 20:34:31 -0700

    update spacing

--- src/components/SectionWork.vue
+++ src/components/SectionWork.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-    <section class="section-contact flex-center">
+    <section class="section-contact flex-center px-32">
         <article class="section-content">
             <Icon 
                 aria-label="seedling decoration icon" 
@@ -11,11 +11,11 @@
                 size="fa-3x" 
                 class="mb-16" />
             <SectionTitle text="Check out my playground!" />
-            <div class="section-copy px-32 mb-40">
+            <div class="section-copy px-16 mb-40">
                 <p>A little sandbox where I try out new things!</p>
                 <p>My goal is to create my own design system that may power up my future projects</p>
                 <p>This is still a WIP, but feel free to <a href="https://sweet-duckling.vercel.app/" title="Joanna playground page" target="_blank">look around!</a> <span>&#9829;</span></p>
-                <p>
+                <p class="mt-32">
                     The current tech stack in this playground: <span class="text-bold">ReactJs + Typescript + Vite</span>
                 </p>
             </div>
